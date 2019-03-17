455f5fc41ad165e82411cdbeca615c9986ad6abb
nabilspann <nabilspann@gmail.com>
2019-03-16 19:34:05 -0500

    adding background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 // import "./App.css";
 import Game from "./chess/Game";
+import { Helmet } from "react-helmet";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <title>Nabil's Web</title>
+          {/* <style type="text/css">{`
+        body {
+            background-color: black;
+        }
+
+        
+    `}</style> */}
+        </Helmet>
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -20,11 +31,11 @@ class App extends Component {
             Learn React
           </a>
         </header> */}
-        <div>
-          <p className="welcome-statement">
+        <div className="welcome-statement">
+          <p>
             Welcome to my website. I'm Nabil Spann, a developer using ReactJS to
-            build websites. Currently I am making playable chess puzzles on my
-            website. Please check them out!
+            build websites. Playable chess puzzles are under development. Come
+            back to challenge them soon!
           </p>
         </div>
         {/* <Game /> */}
